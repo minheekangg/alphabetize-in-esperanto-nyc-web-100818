d4f3eb2204a7fbceb14c294d81769bf0eea321d1
Min Hee  Park <minheekangg@gmail.com>
2018-10-05 21:19:32 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,9 @@
 def alphabetize(arr)
-  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
   arr.sort_by do |words|
     arr.split("").map do |character|
-      alphabet.index(character)
+      esperanto.index(character)
     end
   end
 end
